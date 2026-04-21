22c76af9833357ea3114a1def823eba49a55ca2c
nikhilsharma200904-ops <nikhilsharma200904@gmail.com>
2026-04-21 10:44:26 +0530

    Fix view engine setting in math.js

--- backend/math.js
+++ backend/math.js
@@ -3,7 +3,7 @@ let path=require('path');
 let app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
+app.set('views',path.join(__dirname,'views'));
 
 app.listen(4000,()=>{
     console.log("server is running at 4000")
